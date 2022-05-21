ba6024e442ea1eb2cdcd71e2197fa53858fe1ed4
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2022-05-21 14:08:21 +0200

    added prettier config, graphql apollo connection, base files, mixins and more

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from "vue";
+// Imports
+import { createApp, h } from "vue";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { createApolloProvider } from "@vue/apollo-option";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the Apollo client
+const apolloClient = new ApolloClient({
+  cache,
+  uri: "http://localhost:3001/api/graphql",
+});
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+});
+
+// Create and mount the app
+const app = createApp({
+  render: () => h(App),
+});
+
+app.use(apolloProvider);
+
+app.mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<template>
+  <h1>Home</h1>
+</template>
+
+<style lang="scss">
+h1 {
+  font-size: $fontSize80;
+  font-weight: $fontWeightBold;
+
+  @include responsive(tablet) {
+    color: lightcoral;
+  }
+
+  @include responsive(desktop) {
+    color: lightblue;
+  }
+}
+</style>
