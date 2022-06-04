391b5a50bdb797f312cb3be19d1553caf56cb475
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2022-06-05 00:57:05 +0200

    added content to the contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,19 +4,30 @@ import { useMeta } from 'vue-meta';
 import BaseLayout from '../layouts/BaseLayout.vue';
 import ContactForm from '../components/forms/ContactForm.vue';
 import FaqSection from '@/components/faqs/FaqSection.vue';
+import TitleAndText from '../components/TitleAndText.vue';
 
 export default {
   name: 'Contact',
-  components: { BaseLayout, FaqSection, ContactForm },
+  components: { BaseLayout, FaqSection, ContactForm, TitleAndText },
   setup() {
-    useMeta({ title: 'Contact' });
+    useMeta({ 
+        title: 'Contact',
+        description: `Don't be a stranger and say hello to us! We are always happy to hear from you.`,
+      });
   },
 };
 </script>
 
 <template>
   <BaseLayout>
-    <h1>Say hello</h1>
+    <TitleAndText
+      bgImageUrl="https://res.cloudinary.com/dvb6lcmag/image/upload/v1653846499/bg-images/Yellow_Paint_byfsl6.png"
+      type="h1"
+      title="Contact"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </TitleAndText>
 
     <ContactForm />
 
