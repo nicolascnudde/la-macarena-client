be96930f379ba738cdde8a5dbd5b9f95f348a40e
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2022-06-05 19:59:42 +0200

    added the our story section as a component

---
+++ src/components/our-story/OurStory.vue
@@ -0,0 +1,122 @@
+<script>
+import TitleAndText from '../TitleAndText.vue';
+
+export default {
+  name: 'Our Story',
+  components: { TitleAndText },
+  props: {
+    title: {
+      type: String,
+      default: 'Our Story',
+    },
+    description: {
+      type: String,
+      default:
+        'This is the description of the our story section. Placeholder text until the description is loaded in from the cms.',
+    },
+    imageUrl: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/dvb6lcmag/image/upload/v1653469604/categories/cl3ld3z9j00012062ah2abmp1.webp',
+    },
+  },
+};
+</script>
+
+<template>
+  <section class="our-story">
+    <div class="our-story__container container">
+      <div class="our-story__background"></div>
+
+      <div class="our-story__image">
+        <img :src="imageUrl" />
+
+        <img :src="imageUrl" />
+      </div>
+
+      <div class="our-story__content">
+        <TitleAndText
+          type="h2"
+          :title="title"
+          bgImageUrl="https://res.cloudinary.com/dvb6lcmag/image/upload/v1653846485/bg-images/Brush_Pink_dsvgal.png"
+        >
+          {{ description }}
+        </TitleAndText>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.our-story {
+  margin-bottom: 5rem;
+
+  @include responsive(tablet) {
+    margin-bottom: 8rem;
+  }
+
+  @include responsive(desktop) {
+    position: relative;
+  }
+
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__background {
+    display: none;
+
+    @include responsive(desktop) {
+      display: block;
+      background: $clrRed;
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      height: 20rem;
+      width: 48%;
+      border-radius: 0 25px 25px 0;
+    }
+  }
+
+  &__image {
+    display: none;
+
+    @include responsive(desktop) {
+      display: block;
+      flex-basis: 48%;
+      position: relative;
+      padding: 10rem 0;
+
+      img {
+        width: 15rem;
+        position: absolute;
+
+        &:first-child {
+          top: 3rem;
+          right: 6rem;
+          border-radius: 20px 40px 30px 10px;
+        }
+
+        &:last-child {
+          bottom: 3rem;
+          right: 12rem;
+          border-radius: 30px 20px 40px 10px;
+        }
+      }
+    }
+  }
+
+  &__content {
+    @include responsive(desktop) {
+      flex-basis: 48%;
+    }
+
+    .title-text {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -6,10 +6,11 @@ import Hero from '@/components/hero/Hero.vue';
 import TeamSection from '@/components/team/TeamSection.vue';
 import TitleAndText from '@/components/TitleAndText.vue';
 import Usps from '@/components/usps/Usps.vue';
+import OurStory from '../components/our-story/OurStory.vue';
 
 export default {
   name: 'About',
-  components: { BaseLayout, Hero, TitleAndText, Usps, TeamSection },
+  components: { BaseLayout, Hero, TitleAndText, Usps, TeamSection, OurStory },
   setup() {
     useMeta({ title: 'About' });
   },
@@ -22,6 +23,8 @@ export default {
       title="About us"
       description="This is a dummy description for the hero section on the about page. This will be replaced with a real description from the CMS."
     />
+
+    <OurStory />
 
     <Usps />
 
