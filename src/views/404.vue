cd0e9d4cedcf26609d7a1966b4f8a84feaf3588b
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2022-06-10 00:28:47 +0200

    updated the 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -2,32 +2,69 @@
 import { useMeta } from 'vue-meta';
 
 import BaseLayout from '../layouts/BaseLayout.vue';
+import Illustration404 from '@/assets/icons/404.svg?component';
+import AppButton from '../components/button/AppButton.vue';
 
 export default {
   name: '404',
-  components: { BaseLayout },
+  components: { AppButton, BaseLayout, Illustration404 },
   setup() {
-    useMeta({ title: '404' });
+    useMeta({ title: '404 - Page not found' });
   },
 };
 </script>
 
 <template>
-  <BaseLayout>
-    <h1>404</h1>
+  <BaseLayout pageClass="page-not-found">
+    <div class="container">
+      <h1 class="page-not-found__title">Page not found</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
+      <AppButton
+        class="page-not-found__button"
+        link="/"
+      >
+        Go back home
+      </AppButton>
 
-    <router-link to="/">Home</router-link>
+      <div class="page-not-found__image">
+        <img
+          src="https://res.cloudinary.com/dvb6lcmag/image/upload/v1654813030/404/404_akmnof.png"
+          alt="page not found"
+        />
+      </div>
+    </div>
   </BaseLayout>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.page-not-found {
+  &__title {
+    margin-bottom: 2rem;
+
+    @include responsive(desktop) {
+      margin-bottom: 3rem;
+    }
+  }
+
+  &__button {
+    margin-bottom: 3rem;
+
+    @include responsive(desktop) {
+      margin-bottom: 0;
+    }
+  }
+
+  &__image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 5rem;
+
+    @include responsive(desktop) {
+      img {
+        width: 50%;
+      }
+    }
+  }
+}
+</style>
