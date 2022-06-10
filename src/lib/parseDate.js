c286ea4a32b401ddd70b6cc6d258209066af03f6
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2022-06-10 15:38:58 +0200

    removed the lib folder

--- src/lib/parseDate.js
+++
@@ -1,9 +0,0 @@
-function parseDate (date) {
-  const parsedDate = new Date(date);
-  const month = parsedDate.getMonth() + 1;
-  const day = parsedDate.getDate();
-
-  return `${day}/${month}`;
-};
-
-export default parseDate;
